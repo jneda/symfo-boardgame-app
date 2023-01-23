4e5ef4706ccb27feb695416db9c4f6a4df392042
jdane <julien.dane@gmail.com>
2023-01-23 16:18:52 +0100

    feat: use FileUpload service

--- src/Controller/BoardGameController.php
+++ src/Controller/BoardGameController.php
@@ -4,15 +4,15 @@ namespace App\Controller;
 
 use App\Entity\BoardGame;
 use App\Form\BoardGameType;
+use App\Service\FileUploader;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
-use Symfony\Component\Validator\Constraints\Uuid;
 
 class BoardGameController extends AbstractController
 {
@@ -29,7 +29,7 @@ class BoardGameController extends AbstractController
     }
 
     #[Route('/boardgame/new', name: 'app_new_boardgame')]
-    public function new(Request $request, ManagerRegistry $doctrine, SluggerInterface $slugger): Response
+    public function new(Request $request, ManagerRegistry $doctrine, FileUploader $fileUploader): Response
     {
         $boardGame = new BoardGame;
 
@@ -42,32 +42,22 @@ class BoardGameController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $boardGame = $form->getData();
 
-            /** @var UploadeFile $imageFile */
+            /** @var UploadedFile $imageFile */
             $imageFile = $form->get('image')->getData();
 
             if ($imageFile) {
-                $fileName = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
-                $safeFileName = $slugger->slug($fileName);
-                $newFileName = $safeFileName . '-' . uniqid() . '.' . $imageFile->guessExtension();
-                dump($newFileName);
+                $imageFileName = $fileUploader->upload($imageFile);
             }
 
-            try {
-                $imageFile->move($this->getParameter('images_directory'), $newFileName);
-            } catch (FileException $e) {
-                dump($e);
-            }
+            $boardGame->setImage($imageFileName);
 
-            $boardGame->setImage($newFileName);
-            
             $entityManager = $doctrine->getManager();
             $entityManager->persist($boardGame);
             $entityManager->flush();
- 
+
             $this->addFlash('success', 'Votre jeu a été enregistré !');
 
             return $this->redirectToRoute('app_board_game');
-            
         }
 
         return $this->render('board_game/new.html.twig', [
